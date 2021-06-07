3b577a4c8f53a4d3fa575ca5761cddf1261985d2
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-06-07 13:05:18 +0800

    building the model relationship and learn to seed example data

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,52 +2,54 @@
 const bcrypt = require('bcrypt');
 const config = require('../config/app')
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    avatar: {
-      type: DataTypes.STRING,
-      get() {
-        const avatar = this.getDataValue('avatar');
-        const url = `${config.appUrl}:${config.appPort}`
-        if (!avatar) {
-          return `${url}/${this.getDataValue('gender')}.svg`;
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.belongsToMany(models.Chat, {through: 'ChatUser', foreignKey: 'userId'})
+            this.hasMany(models.ChatUser, {foreignKey: 'userId'})
         }
-        const id = this.getDataValue('id')
-        return `${url}/user/${id}/${avatar}`
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    hooks: {
-      beforeCreate: hashPassword,
-      beforeUpdate: hashPassword
-    }
-  });
-  return User;
+    };
+    User.init({
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        gender: DataTypes.STRING,
+        avatar: {
+            type: DataTypes.STRING,
+            get() {
+                const avatar = this.getDataValue('avatar');
+                const url = `${config.appUrl}:${config.appPort}`
+                if (!avatar) {
+                    return `${url}/${this.getDataValue('gender')}.svg`;
+                }
+                const id = this.getDataValue('id')
+                return `${url}/user/${id}/${avatar}`
+            }
+        }
+    }, {
+        sequelize,
+        modelName: 'User',
+        hooks: {
+            beforeCreate: hashPassword,
+            beforeUpdate: hashPassword
+        }
+    });
+    return User;
 };
 
 const hashPassword = async (user) => {
-  if (user.changed('password')) {
-    user.password = await bcrypt.hash(user.password, 10)
-  }
+    if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 10)
+    }
 
-  return user
+    return user
 }
