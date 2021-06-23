b56354b990dc799fd60e0d950861a2d49552f9df
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-06-23 22:13:08 +0800

    push some modules

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({User, ChatUser, Message}) {
       // define association here
-      this.belongsToMany(models.User, {through: 'ChatUser', foreignKey: 'chatId'});
-      this.hasMany(models.ChatUser, {foreignKey: 'chatId'})
-      this.hasMany(models.Message, {foreignKey: 'chatId'})
+      this.belongsToMany(User, {through: 'ChatUser', foreignKey: 'chatId'});
+      this.hasMany(ChatUser, {foreignKey: 'chatId'})
+      this.hasMany(Message, {foreignKey: 'chatId'})
     }
   };
   Chat.init({
--- backend/router/chat.js
+++ backend/router/chat.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const {index} = require("../controllers/chatController");
-const {validate} = require("../validators");
+const {index, create, messages, deleteChat} = require("../controllers/chatController");
 const {auth} = require('../middleware/auth')
 
-router.get('/', [auth], index)
+router.get('/', [auth], index);
+router.get('/messages', [auth], messages);
+router.post('/create', [auth], create);
+router.delete('/:id', [auth], deleteChat);
 
 module.exports = router;
